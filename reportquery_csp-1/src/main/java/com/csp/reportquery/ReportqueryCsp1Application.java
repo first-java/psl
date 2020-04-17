7887dfd988c7d3aacacb64596134541950eee55a
奥特虾不会写代码 <@DESKTOP-11AKJED>
2020-04-17 08:01:34 +0800

    新建CSP派料报表查询项目

---
+++ reportquery_csp-1/src/main/java/com/csp/reportquery/ReportqueryCsp1Application.java
@@ -0,0 +1,13 @@
+package com.csp.reportquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportqueryCsp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportqueryCsp1Application.class, args);
+	}
+
+}
---
+++ reportquery_csp-1/src/test/java/com/csp/reportquery/ReportqueryCsp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csp.reportquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportqueryCsp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
